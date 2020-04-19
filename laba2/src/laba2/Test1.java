6cce66cbaa7f87ae2d0c7409a768cef7f0786d4d
18130319 <18130319>
2020-04-19 18:32:02 +0500

    Перенос репозитория

---
+++ laba2/src/laba2/Test1.java
@@ -0,0 +1,15 @@
+package laba2;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Test1 {
+
+@Test
+void test() {
+assertEquals("The method should be invoked 3 times", 3, 3);
+}
+
+}
